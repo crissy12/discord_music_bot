f38c126fc57fabddb5a3260fadb96c6c1539f828
Cristian <cristiangcs45@gmail.com>
2021-12-21 10:14:30 -0500

    Working queue with async send

--- music_bot.py
+++ music_bot.py
@@ -18,6 +18,7 @@ bot = commands.Bot(command_prefix='!',intents=intents)
 yt_dlp.utils.bug_reports_message = lambda: ''
 
 song_queue = []
+url_list=[]
 
 def emptydir():
     for item in os.listdir(os.getcwd() + '/downloaded_music'):
@@ -60,7 +61,7 @@ class YTDLSource(discord.PCMVolumeTransformer):
         filename = data['title'] if stream else ytdl.prepare_filename(data)
         return filename
 
-# implement queue
+# implement queue (gf comment never remove)
 
 @bot.command(name='join', help='Tells the bot to join the voice channel')
 async def join(ctx):
@@ -75,7 +76,9 @@ async def join(ctx):
 async def leave(ctx):
     voice_client = ctx.message.guild.voice_client
     if voice_client.is_connected():
+        voice_client.stop()
         await voice_client.disconnect()
+        await asyncio.sleep(10)
         emptydir()
     else:
         await ctx.send("The bot is not connected to a voice channel.")
@@ -94,35 +97,41 @@ async def play(ctx,url):
     try :
         server = ctx.message.guild
         voice_channel = server.voice_client
-
         async with ctx.typing():
             filename = await YTDLSource.from_url(url, loop=bot.loop)
-            
-            if not song_queue:
-                voice_channel.play(discord.FFmpegPCMAudio(executable="ffmpeg.exe", source=filename))
-                await ctx.send('**Now playing:** {}'.format(url)) 
-            else:
-                start_playing(voice_channel,filename)
-                await ctx.send('Song: ' + url + ' has been added to the queue')
+        song_queue.append(filename)
+        url_list.append(url)
+        await ctx.send('Song has been added to the queue')
+        await start_playing(ctx)
     except Exception as e:
         print(e)
-                  
-def start_playing(voice_channel,filename):
-    i = 0
-    while i < len(song_queue):  
-        try:
-            voice_channel.play(discord.FFmpegPCMAudio(executable="ffmpeg.exe", source=song_queue[i]))
 
-        except:
-            pass
-        i += 1
-          
+async def start_playing(ctx):
+    voice_client = discord.utils.get(bot.voice_clients, guild=ctx.guild)
+    if not voice_client.is_playing(): 
+        try:
+            print('\nThis is the current queue: ' + str(url_list))
+            await ctx.send('**Now playing:** {}'.format(url_list.pop(0))) 
+            voice_client.play(
+                discord.FFmpegPCMAudio(executable="ffmpeg.exe",
+                source=song_queue.pop(0)),
+                after= lambda e: song_starter(ctx))
+        except :
+            pass  
+
+def song_starter(ctx):
+    starter = start_playing(ctx)
+    repeat = asyncio.run_coroutine_threadsafe(starter,client.loop)
+    try:
+        repeat.result()
+    except:
+        pass
 
 @bot.command(name='pause', help='This command pauses the song')
 async def pause(ctx):
     voice_client = ctx.message.guild.voice_client
     if voice_client.is_playing():
-        await voice_client.pause()
+        voice_client.pause()
     else:
         await ctx.send("The bot is not playing anything at the moment.")
     
@@ -130,7 +139,7 @@ async def pause(ctx):
 async def resume(ctx):
     voice_client = ctx.message.guild.voice_client
     if voice_client.is_paused():
-        await voice_client.resume()
+        voice_client.resume()
     else:
         await ctx.send("The bot was not playing anything before this. Use play_song command")
 
---
+++ test.py
@@ -0,0 +1,12 @@
+test = [[1,2] ,[3,4]]
+test.append([4,5])
+
+print (test)
+
+print (test.pop(0)[1])
+
+print (test)
+
+print (test.pop(0)[1])
+
+print (test)
